d4f730e61b8517eab8b2971f6bd15cd926b72b4f
Owais Sayyed <mowais.sayyed@gmail.com>
2024-08-02 16:18:22 +0530

    Third Part Done

--- frontend/src/components/Siderbar.jsx
+++ frontend/src/components/Siderbar.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import SearchInput from "./SearchInput";
-import Conversions from "./Conversions";
+
 import LogoutButton from "./LogoutButton";
+import Conversations from "./Conversations";
 
 const Siderbar = () => {
   return (
     <div className="border-r border-slate-500 p-4 flex flex-col">
       <SearchInput />
       <div className="divider px-3"></div>
-      <Conversions />
+      <Conversations />
       <LogoutButton />
     </div>
   );
