9d278a2de641f450c31fc4574a3df0c74a8aa94e
Owais Sayyed <mowais.sayyed@gmail.com>
2024-08-04 19:44:29 +0530

    4th part completed - socket

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,11 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
-const app = express();
+// const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
@@ -22,7 +23,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server Running at ${PORT}`);
 });
